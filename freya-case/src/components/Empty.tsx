dbb9cc3e10a9adbe3a81a73d5f7ffdf22e52dc9c
hozat <halil.ozat@hepsiburada.com>
2024-06-03 00:17:48 +0300

    added empty data component

---
+++ freya-case/src/components/Empty.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Empty.module.css'
+
+export function Empty() {
+
+    return (
+        <div className={styles.empty}>
+            <div className={styles.emptyText}>
+                Görüntülenecek Ürün Bulunmamaktadır!
+            </div>
+        </div>
+    )
+}
